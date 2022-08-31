a1722d73e0eda2978f367b670906992022bfb7e6
Alfred-Lau <jian.liu@icloud.com>
2022-08-31 23:25:37 +0800

    feat: 添加 error 插件

---
+++ packages/core/src/plugins/error.ts
@@ -0,0 +1,20 @@
+import { log } from "@tg/utils";
+import Tracer from "../tracer";
+
+// 进行页面性能采集上报
+function calcPagePerformance(this:any,e: Event): any {
+  e.preventDefault();
+  // @ts-ignore
+  log.info(e.target!.tagName);
+  this.call("performance");
+}
+
+export default function (instance: Tracer, cb: handleType) {
+  //1. 挂载相关绑定事件
+
+  const fnWrapper = (e: Event) => {
+    calcPagePerformance.call(instance, e);
+  };
+  instance.addEventListener(window, "error", fnWrapper);
+  cb && cb();
+}
